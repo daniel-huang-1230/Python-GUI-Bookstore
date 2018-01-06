61339d6a95e0e6fd985c5a9e1df8eb5641f62ec9
Daniel Huang <gn03249822@DanieltekiMacBook-Pro.local>
2018-01-05 22:07:54 -0800

    backend done

--- frontend.py
+++ frontend.py
@@ -1,6 +1,19 @@
 from tkinter import *
 import backend
 
+def get_selected_row(event):   #the "event" parameter is needed b/c we've binded this function to the listbox
+    index = list1.curselection()[0]
+    global selected_tuple
+    selected_tuple = list1.get(index)
+    e1.delete(0,END)
+    e1.insert(END,selected_tuple[1])
+    e2.delete(0, END)
+    e2.insert(END,selected_tuple[2])
+    e3.delete(0, END)
+    e3.insert(END,selected_tuple[3])
+    e4.delete(0, END)
+    e4.insert(END,selected_tuple[4])
+
 def view_command():
     list1.delete(0, END)  # make sure we've cleared all entries in the listbox every time we press the View all button
     for row in backend.view():
@@ -11,10 +24,20 @@ def search_command():
     for row in backend.search(title_text.get(), author_text.get(), year_text.get(), ISBN_text.get()):
         list1.insert(END, row)
 
-def insert_command():
+def add_command():
     backend.insert(title_text.get(), author_text.get(), year_text.get(), ISBN_text.get())
     list1.delete(0, END)
     list1.insert(END, (title_text.get(), author_text.get(), year_text.get(), ISBN_text.get()))
+
+def delete_command():
+    backend.delete(selected_tuple[0])
+    view_command()
+
+def update_command():
+    #be careful for the next line ---> we are updating using the texts in the entries, not the selected tuple
+    backend.update(selected_tuple[0],title_text.get(), author_text.get(), year_text.get(), ISBN_text.get())
+    view_command()
+
 #code for the GUI (front end)
 window = Tk()
 
@@ -50,6 +73,9 @@ e4.grid(row = 1, column = 3)
 list1 = Listbox(window, height = 6, width = 35)
 list1.grid(row = 2, column =0, rowspan = 6, columnspan = 2)
 
+list1.bind('<<ListboxSelect>>',get_selected_row)
+
+
 #now we need to attach a scrollbar to the listbox, and the other direction,too
 sb1 = Scrollbar(window)
 sb1.grid(row = 2, column = 2, rowspan = 6)
@@ -62,13 +88,13 @@ b1.grid(row = 2, column = 3)
 b2 = Button(window, text = "Search entry", width = 12, command = search_command)
 b2.grid(row = 3, column = 3)
 
-b3 = Button(window, text = "Add entry", width = 12, command = insert_command)
+b3 = Button(window, text = "Add entry", width = 12, command = add_command)
 b3.grid(row = 4, column = 3)
 
-b4 = Button(window, text = "Update selected", width = 12)
+b4 = Button(window, text = "Update selected", width = 12, command = update_command)
 b4.grid(row = 5, column = 3)
 
-b5 = Button(window, text = "Delete selected", width = 12)
+b5 = Button(window, text = "Delete selected", width = 12, command = delete_command)
 b5.grid(row = 6, column = 3)
 
 b6 = Button(window, text = "Close", width = 12)
